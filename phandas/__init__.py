da81da99049fbd5c6b2a5afed74084d499565e7e
quantbai <quantbai@gmail.com>
2025-11-22 23:26:26 +0800

    Fix data download bug

--- phandas/__init__.py
+++ phandas/__init__.py
@@ -3,7 +3,7 @@ A multi-factor quantitative trading framework for cryptocurrency markets.
 """
 
 __author__ = "Phantom Management"
-__version__ = "0.14.0"
+__version__ = "0.14.1"
 
 from .core import Factor
 from .panel import Panel
